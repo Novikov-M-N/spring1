1474dfc93886a66255fe0c24477c6de6ab2b191c
Novikov-M-N <mnovikov37@gmail.com>
2020-10-23 22:28:52 +0700

    lesson7Homework

---
+++ src/main/java/com/github/novikovmn/spring1/Spring1Application.java
@@ -0,0 +1,15 @@
+package com.github.novikovmn.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class Spring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1Application.class, args);
+	}
+
+}
