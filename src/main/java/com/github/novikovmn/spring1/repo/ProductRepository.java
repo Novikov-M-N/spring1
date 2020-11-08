1ba49af6af3b125c49a28a2f997d72f02033ceb7
Novikov-M-N <mnovikov37@gmail.com>
2020-11-08 20:06:57 +0700

    lesson4Homework all is working

--- src/main/java/com/github/novikovmn/spring1/repo/ProductRepository.java
+++ src/main/java/com/github/novikovmn/spring1/repo/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.github.novikovmn.spring1.repo;
 
 import com.github.novikovmn.spring1.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,8 +11,22 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-//    List<Product> testSelect();
 
-    @Query("select p from Product p")
-    public List<Product> testSelect2();
+    // Фильтрация по минимальной цене
+    List<Product> findByCostGreaterThanEqual(Integer cost);
+
+    // Фильтрация по максимальной цене
+    List<Product> findByCostLessThanEqual(Integer cost);
+
+    // Фильтрация по минимальной и максимальной цене
+    List<Product> findByCostBetween(Integer minCost, Integer maxCost);
+
+    // Фильтрация по минимальной цене, постраничное отображение
+    Page<Product> findByCostGreaterThanEqual(Integer minCost, Pageable pageable);
+
+    // Фильтрация по максимальной цене, постраничное отображение
+    Page<Product> findByCostLessThanEqual(Integer maxCost, Pageable pageable);
+
+    // Фильтрация по минимальной и максимальной цене, постраничное отображение
+    Page<Product> findByCostBetween(Integer minCost, Integer maxCost, Pageable pageable);
 }
--- src/main/java/com/github/novikovmn/spring1/service/ProductServiceImpl.java
+++ src/main/java/com/github/novikovmn/spring1/service/ProductServiceImpl.java
@@ -3,10 +3,11 @@ package com.github.novikovmn.spring1.service;
 import com.github.novikovmn.spring1.entities.Product;
 import com.github.novikovmn.spring1.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,22 +15,40 @@ public class ProductServiceImpl implements ProductService{
     @Autowired
     private ProductRepository productRepository;
 
-//    @Autowired
-//    public void setProductRepository(ProductRepository productRepository) {
-//        this.productRepository = productRepository;
-//    }
-
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> getAll() { return productRepository.findAll(); }
 
     @Override
     @Transactional(readOnly = true)
-    public List<Product> getAll() {
+    public List<Product> getByMinCost(Integer minCost) { return productRepository.findByCostGreaterThanEqual(minCost); }
 
-        List<Product> products = new ArrayList<>();
-        Product product = new Product();
-        product.setTitle("Новый");
-        product.setCost(100);
-        products.add(product);
-//        return products;
-        return productRepository.findAll();
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> getByMaxCost(Integer maxCost) { return productRepository.findByCostLessThanEqual(maxCost); }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> getByMinAndMaxCost(Integer minCost, Integer maxCost) {
+        return productRepository.findByCostBetween(minCost, maxCost);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Product> getAllPageable(Pageable pageable) { return productRepository.findAll(pageable); }
+
+    @Override
+    public Page<Product> getByMinCostPageable(Integer minCost, Pageable pageable) {
+        return productRepository.findByCostGreaterThanEqual(minCost, pageable);
+    }
+
+    @Override
+    public Page<Product> getByMaxCostPageable(Integer maxCost, Pageable pageable) {
+        return productRepository.findByCostLessThanEqual(maxCost, pageable);
+    }
+
+    @Override
+    public Page<Product> getByMinAndMaxCostPageable(Integer minCost, Integer maxCost, Pageable pageable) {
+        return productRepository.findByCostBetween(minCost, maxCost, pageable);
     }
 }
